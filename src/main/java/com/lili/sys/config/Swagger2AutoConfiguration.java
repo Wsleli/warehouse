79c61bbb5e00f7efc490a8be784a9803cece1eae
wsleli <aiyingggg@gmail.com>
2024-07-24 17:07:31 +0800

    API文档管理

--- src/main/java/com/lili/sys/config/Swagger2AutoConfiguration.java
+++ src/main/java/com/lili/sys/config/Swagger2AutoConfiguration.java
@@ -17,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2 //启用Swagger2
+// http://localhost:8888/swagger-ui.html
 public class Swagger2AutoConfiguration {
 
     /**
